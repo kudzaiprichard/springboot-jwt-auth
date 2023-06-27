6b99f2bb47f271fa09ee3fd1b4ffd7453512924a
Kudzai P Matizirofa <kudzaiprichard@gmail.comC:/Users/Kudzai Prichard/AppData/Local/Programs/Python/Python310-32/python.exe>
2023-06-27 10:05:01 +0200

    added User repository

---
+++ src/main/java/com/intela/dpringjwtauth/Repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.intela.dpringjwtauth.Repositories;
+
+import com.intela.dpringjwtauth.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
