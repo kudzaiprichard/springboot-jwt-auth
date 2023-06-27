bafbf8a0d8581f79d735fbcae1b88647b5a0d3df
Kudzai P Matizirofa <kudzaiprichard@gmail.comC:/Users/Kudzai Prichard/AppData/Local/Programs/Python/Python310-32/python.exe>
2023-06-27 09:58:46 +0200

    added a User and Role Model

---
+++ src/main/java/com/intela/dpringjwtauth/models/Role.java
@@ -0,0 +1,6 @@
+package com.intela.dpringjwtauth.models;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/com/intela/dpringjwtauth/models/User.java
+++ src/main/java/com/intela/dpringjwtauth/models/User.java
@@ -5,14 +5,20 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "_users")
-public class User {
+@Table(name = "Users")
+public abstract class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,5 +26,37 @@ public class User {
     private String firstName;
     private String lastName;
     private String email;
-    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
